aebe675af3505f74070c4cfe0b646b2d21477c5e
Iznaur <PersonalC0019@gmail.com>
2026-01-09 10:36:39 +0300

    Change main.go

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := 5
+
+	//Сложение
+	sum := a + b
+	fmt.Println("Сумма:", sum)
+
+	//Вычитание
+	diff := a - b
+	fmt.Println("Разность:", diff)
+
+	//Умножение
+	product := a * b
+	fmt.Println("Произведение:", product)
+
+	//Деление
+	quotient := a / b
+	fmt.Println("Частное:", quotient)
+}
